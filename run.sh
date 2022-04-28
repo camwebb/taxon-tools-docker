b72927384f95baff568d01c0dc63533ebeb63ce9
Cam Webb <cw@camwebb.info>
2022-04-27 17:05:37 -0800

    Working!

--- run.sh
+++ run.sh
@@ -7,6 +7,10 @@ docker build -t matchnames .
 
 docker run --rm -it -v `pwd`:`pwd` -w `pwd` matchnames -a listA -b listB -o output
 
+# Manual, intereactive:
+# docker run --rm -it -v `pwd`:`pwd` -w `pwd` matchnames -a listA -b listB -o test -f
+
+
+
 # Cool:
 #   docker run -i -t archlinux /bin/bash
-# docker images
